bb8bd13997ece258e674cb573c3cfb05d5b3faf9
lithirtyacre <liamacre@gmail.com>
2015-02-24 17:20:34 -0800

    rmjoke

--- gist/clicks.js
+++ gist/clicks.js
@@ -1,3 +1,14 @@
 /*global $*/
 /*jslint sloppy:true, browser: true, white: true*/
 $("h1.page-title").text("my code runs!");
+$(window).on('click', function (e) {
+    $('img.logo').css({transition: 1 s});
+});
+$(window).on('click', function (e) {
+    $('.logo').css({left: e.pageX,
+                    top: e.pageY});
+    $('.logo').toggleClass('rotated');
+});
+.rotated {
+    transform: rotate(180deg);
+}
